16ff05ddeaf4b86f87f6313efaf959524f61d3e7
Ulizinho <perezulises13@hotmail.com>
2025-03-04 16:31:10 -0600

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ['PlayChickens', 'sans-serif'],
+        super: ['SuperFeel', 'sans-serif'],
+        sla: ['Slaberlin', 'sans-serif'],
+        slabold: ['SlaberlinBold', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
