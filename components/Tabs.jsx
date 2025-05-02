a94ba3f55cfa8841bc35ce82e1eedc09bd2369b2
Ulizinho <perezulises13@hotmail.com>
2025-05-02 02:45:57 -0600

    Rompecabezas en menú guía

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -1,6 +1,5 @@
-import { View, Text, Pressable, Keyboard } from "react-native";
+import { View, Text, Pressable, Keyboard, Image } from "react-native";
 import { Feather } from "@expo/vector-icons";
-import { FontAwesome6 } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
 import { useRoute } from "@react-navigation/native";
@@ -11,27 +10,22 @@ export default function Tabs() {
   const [isOpen, setOpen] = useState(false);
 
   useEffect(() => {
-    // Suscribirse a los eventos de teclado
-    const keyboardDidShowListener = Keyboard.addListener(
-      "keyboardDidShow",
-      () => {
-        setOpen(true);
-      }
-    );
+    const keyboardDidShowListener = Keyboard.addListener("keyboardDidShow", () => {
+      setOpen(true);
+    });
 
-    const keyboardDidHideListener = Keyboard.addListener(
-      "keyboardDidHide",
-      () => {
-        setOpen(false);
-      }
-    );
+    const keyboardDidHideListener = Keyboard.addListener("keyboardDidHide", () => {
+      setOpen(false);
+    });
 
-    // Limpiar los listeners cuando el componente se desmonte
     return () => {
       keyboardDidShowListener.remove();
       keyboardDidHideListener.remove();
     };
   }, []);
+
+  const isEmotionsActive =
+    route.name === "emotions" || route.name === "galery" || route.name === "report";
 
   return (
     <View
@@ -39,19 +33,17 @@ export default function Tabs() {
         isOpen ? "hidden" : ""
       }`}
     >
-      <View className="flex items-center justify-between flex-center flex-row">
-        <Pressable
-          onPress={() => router.navigate("/home")}
-          className="flex items-center"
-        >
+      <View className="flex items-center justify-between flex-row">
+        {/* Inicio */}
+        <Pressable onPress={() => router.navigate("/home")} className="flex items-center">
           <Feather
             name="home"
             size={28}
-            color={route.name == "home" ? "#0369a1" : "rgb(107,114,128)"}
+            color={route.name === "home" ? "#0369a1" : "rgb(107,114,128)"}
           />
           <Text
             className={`${
-              route.name == "home"
+              route.name === "home"
                 ? "text-sky-800 font-slabold"
                 : "text-gray-500 font-slabold"
             } text-center`}
@@ -59,26 +51,22 @@ export default function Tabs() {
             Inicio
           </Text>
         </Pressable>
+
         <Pressable
           onPress={() => router.navigate("/emotions")}
-          className="flex items-center justify-center text-center"
+          className="flex items-center justify-center"
         >
-          <FontAwesome6
-            name="face-smile-beam"
-            size={28}
-            color={
-              route.name == "emotions" ||
-              route.name == "galery" ||
-              route.name == "report"
-                ? "#0369a1"
-                : "rgb(107,114,128)"
-            }
+          <Image
+            source={require("../assets/images/SCANTEATE LOGO FIGURA.png")}
+            style={{
+              width: 33,
+              height: 33,
+            }}
+            resizeMode="contain"
           />
           <Text
             className={`${
-              route.name == "emotions" ||
-              route.name == "galery" ||
-              route.name == "report"
+              isEmotionsActive
                 ? "text-sky-800 font-slabold"
                 : "text-gray-500 font-slabold"
             } text-center`}
@@ -86,18 +74,17 @@ export default function Tabs() {
             Emociones
           </Text>
         </Pressable>
-        <Pressable
-          onPress={() => router.navigate("/settings")}
-          className="flex items-center justify-center text-center"
-        >
+
+        {/* Mi Perfil */}
+        <Pressable onPress={() => router.navigate("/settings")} className="flex items-center">
           <MaterialCommunityIcons
             name="account-circle-outline"
             size={28}
-            color={route.name == "settings" ? "#0369a1" : "rgb(107,114,128)"}
+            color={route.name === "settings" ? "#0369a1" : "rgb(107,114,128)"}
           />
           <Text
             className={`${
-              route.name == "settings"
+              route.name === "settings"
                 ? "text-sky-800 font-slabold"
                 : "text-gray-500 font-slabold"
             } text-center`}
